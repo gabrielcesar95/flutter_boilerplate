b59bfc0b1ebe9ae3571c426245049c065e273b5d
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2020-07-26 17:06:01 -0300

    access token interceptor

--- lib/app/http/api/client.dart
+++ lib/app/http/api/client.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter_boilerplate/app/http/api/oauth_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract class Api {
   Dio client;
   String path;
+
+  Future<String> _token = OauthService().getMobileToken();
 
   BaseOptions options = BaseOptions(
     baseUrl: DotEnv().env['APP_URL'],
@@ -11,11 +16,28 @@ abstract class Api {
     receiveTimeout: 5000,
     headers: {
       'Accept': 'application/json',
-      'Authorization': 'Bearer token'
     },
   );
 
   Api() {
     client = new Dio(options);
+
+    client.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options) async {
+        String token = await this._token;
+
+        if (token != null && !options.headers.containsKey('Authorization')) {
+          options.headers = {'Authorization': 'Bearer ${await getAccessToken()}'};
+        }
+
+        return options;
+      },
+    ));
+  }
+
+  getAccessToken() async {
+    Map credentials = jsonDecode(await this._token);
+
+    return credentials['accessToken'];
   }
 }
